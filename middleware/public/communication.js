85fc8e442e0fa14b9b92a6465929933ad93db5bd
augusticor <rojascruzoscar@gmail.com>
2020-12-11 11:32:21 -0500

    Se agrega interfaz base y nodemailer al package.json
    - Se agrega nodemailer a package.json ya que antes no estaba
    - Se añade index.html
    - Una imagen quemada en carpeta public

---
+++ middleware/public/communication.js
@@ -0,0 +1,31 @@
+var quote = new Vue({
+	el: '#quote',
+	data: {
+		quote: 'Solo que nada sé',
+	},
+});
+
+var servernum = new Vue({
+	el: '#servernum',
+	data: {
+		server: 'Servidor 1',
+	},
+});
+
+var buttonsa = new Vue({
+	el: '#buttons',
+	methods: {
+		frase: () => {
+			quote.data = {
+				quote: 'Nueva frase',
+			};
+			console.log('Aca llamamos el servicio de node para cambiar la frase');
+		},
+		instancia: () => {
+			console.log('/instancia');
+		},
+		email: () => {
+			console.log('/email');
+		},
+	},
+});
