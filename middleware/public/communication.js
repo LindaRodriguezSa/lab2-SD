afc288954d60f18c8776cef1c6546298cdecd9f1
augusticor <rojascruzoscar@gmail.com>
2020-12-11 21:29:56 -0500

    Se agrego el metodo para llamar el envio de correo y se esta haciendo el slider de imgs

--- middleware/public/communication.js
+++ middleware/public/communication.js
@@ -1,3 +1,8 @@
+const slider = document.querySelector('.main-img');
+
+let images = ['img.jpg', 'img.jpg'];
+let index = 0;
+
 let quoteData = {
 	quote: 'Solo sé que nada sé',
 };
@@ -23,10 +28,12 @@ var buttonsa = new Vue({
 			changeQuote();
 		},
 		instancia: () => {
+			console.log('Nuea');
 			createInstance();
+			console.log(images.length);
 		},
 		email: () => {
-			console.log('/email');
+			sendEmail();
 		},
 	},
 });
@@ -38,5 +45,34 @@ function changeQuote() {
 }
 
 function createInstance() {
-	quoteData.quote = changeQuote();
+	images.push('https://skyprep.com/wp-content/uploads/2013/07/Alfred_Quotes1.jpg');
+	// quoteData.quote = changeQuote();
+}
+
+function sendEmail() {
+	fetch('/email');
+	alert('Email enviado');
+}
+
+var arrowButtons = new Vue({
+	el: '#arrow-buttons',
+	methods: {
+		right: () => {
+			nextPicture();
+		},
+		left: () => {
+			previousePicture();
+		},
+	},
+});
+
+function nextPicture() {
+	index = index + 1 >= images.length ? 0 : index + 1;
+	slider.src = images[index];
+}
+
+function previousePicture() {
+	index = index - 1 < 0 ? images.length - 1 : index - 1;
+	slider.alt = 'Nada de nada';
+	slider.src = images[index];
 }
