fbd1f859bb12f371d711d3eb54655cdce3c7bf90
augusticor <rojascruzoscar@gmail.com>
2021-02-22 18:48:57 -0500

    Numero de servidor se muestra correctamente, con carousel
    - El numero de servidor corresponde correctamente a la imagen que
    actualmente se este mostrando
    - Se agrego axios a las dependenicas package.json
    - Se agrega el atributo para conocer el id actual del carousel en
    index.html
    - Se elimina log.txt bug

--- middleware/public/communication.js
+++ middleware/public/communication.js
@@ -2,115 +2,132 @@ let index = 0;
 let servidores;
 
 let quoteData = {
-  quote:
-    "'El sabio no dice nunca todo lo que piensa, pero siempre piensa todo lo que dice (Aristóteles)'",
+	quote: "'El sabio no dice nunca todo lo que piensa, pero siempre piensa todo lo que dice (Aristóteles)'",
 };
 
 let serverData = {
-  server: " ",
+	server: 0,
 };
 
 var compQuote = new Vue({
-  el: "#quote",
-  data: quoteData,
+	el: '#quote',
+	data: quoteData,
 });
 
 var servernum = new Vue({
-  el: "#servernum",
-  data: serverData,
+	el: '#servernum',
+	data: serverData,
 });
 
 var buttonsa = new Vue({
-  el: "#buttons",
-  methods: {
-    frase: () => {
-      changeQuote();
-    },
-    instancia: () => {
-      createInstance();
-    },
-    email: () => {
-      sendEmail();
-    },
-  },
+	el: '#buttons',
+	methods: {
+		frase: () => {
+			changeQuote();
+		},
+		instancia: () => {
+			createInstance();
+		},
+		email: () => {
+			sendEmail();
+		},
+	},
 });
 
+/**
+ * Realiza una petición al servidor para cambiar la frase
+ * @request get
+ */
 function changeQuote() {
-  let newsrc = document.getElementById("image-url").value;
-  if (newsrc == "") {
-    newsrc =
-      "https://images.unsplash.com/photo-1506318137071-a8e063b4bec0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=770&q=80";
-  }
+	// valida que el usuario ingrese un link de una imagen, si no es asi, pone una default
+	let newsrc = document.getElementById('image-url').value;
+	if (newsrc == '') {
+		newsrc =
+			'https://images.unsplash.com/photo-1506318137071-a8e063b4bec0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=770&q=80';
+	}
 
-  imagecomponent.images[index].src = newsrc;
+	imagecomponent.images[index].src = newsrc;
 
-  imagecomponent.image = imagecomponent.images[index];
+	imagecomponent.image = imagecomponent.images[index];
 
-  fetch("http://192.168.0.11:3000/getQuote")
-    .then((response) => response.text())
-    .then((info) => (quoteData.quote = info));
-  fetch("/getServer")
-    .then((response) => response.text())
-    .then((info) => serverData.server.info);
+	fetch('/getQuote')
+		.then((response) => response.text())
+		.then((comingQuote) => {
+			console.log(comingQuote);
+			quoteData.quote = comingQuote;
+		})
+		.catch((error) => console.log(error));
+
+	// fetch("/getServer")
+	//   .then((response) => response.text())
+	//   .then((info) => serverData.server.info);
 }
 
 function createInstance() {
-  fetch("/getInstance");
-  imagecomponent.images.push({
-    index: imagecomponent.images.length + 1,
-    src:
-      "https://i.picsum.photos/id/430/536/354.jpg?hmac=uxrNCXgJuycp2JMZ9jpZb5ThTsZIplRTSPuc4ybMyws",
-  });
+	fetch('/getInstance');
+	imagecomponent.images.push({
+		index: imagecomponent.images.length + 1,
+		src: 'https://i.picsum.photos/id/430/536/354.jpg?hmac=uxrNCXgJuycp2JMZ9jpZb5ThTsZIplRTSPuc4ybMyws',
+	});
 }
 
 function sendEmail() {
-  fetch("/email");
-  alert("email enviado");
-  console.log("nddkd");
+	fetch('/email');
+	alert('email enviado');
+	console.log('nddkd');
 }
 
 var imagecomponent = new Vue({
-  el: "#image-change",
-  data() {
-    return {
-      image: null,
-      images: [
-        {
-          id: 0,
-          src: "https://blog.hubspot.com/hubfs/famous-quotes.jpg",
-        },
-        {
-          id: 1,
-          src:
-            "https://www.planetware.com/wpimages/2019/09/top-places-to-visit-in-the-world-machu-picchu-peru.jpg",
-        },
-        {
-          id: 2,
-          src:
-            "https://cdn.tinybuddha.com/wp-content/uploads/2018/09/Woman-on-a-bench.png",
-        },
-      ],
-    };
-  },
+	el: '#image-change',
+	data() {
+		return {
+			image: null,
+			images: [
+				{
+					id: 0,
+					src: 'https://blog.hubspot.com/hubfs/famous-quotes.jpg',
+				},
+				{
+					id: 1,
+					src: 'https://www.planetware.com/wpimages/2019/09/top-places-to-visit-in-the-world-machu-picchu-peru.jpg',
+				},
+				{
+					id: 2,
+					src: 'https://cdn.tinybuddha.com/wp-content/uploads/2018/09/Woman-on-a-bench.png',
+				},
+			],
+		};
+	},
 });
 
 function rand(n) {
-  return Math.floor(Math.random() * n + 1);
+	return Math.floor(Math.random() * n + 1);
 }
 
 function cambiar() {
-  document.getElementById("ia").src = images[rand(images.length) - 1];
+	document.getElementById('ia').src = images[rand(images.length) - 1];
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const elementosCarousel = document.querySelectorAll(".carousel");
-  M.Carousel.init(elementosCarousel, {
-    duration: 150,
-    dist: -80,
-    shift: 5,
-    padding: 20,
-    numVisible: 3,
-    indicators: true,
-    noWrap: false,
-  });
+document.addEventListener('DOMContentLoaded', () => {
+	const elementosCarousel = document.querySelectorAll('.carousel');
+	M.Carousel.init(elementosCarousel, {
+		duration: 150,
+		dist: -80,
+		shift: 5,
+		padding: 20,
+		numVisible: 3,
+		indicators: true,
+		noWrap: false,
+		// On cycle to permite conocer cuando se mueve el carrusel
+		onCycleTo: (data) => {
+			// data.getAttribute me trae el atributo data-id que tiene el elemento
+			// div con clase .carousel-item, donde almaceno el id actual del carrusel
+			let currentImgID = parseInt(data.getAttribute('data-id'));
+			// finalmente actualizo el index en el que voy
+			// tanto en logica como en ui
+			console.log(typeof currentImgID);
+			index = currentImgID;
+			serverData.server = currentImgID + 1;
+		},
+	});
 });
