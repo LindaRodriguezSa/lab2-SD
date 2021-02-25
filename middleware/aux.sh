c57ae06556ff14386678f810478083022463240e
Linda Rodriguez Salamanca <72898486+limarosa29@users.noreply.github.com>
2021-02-25 11:12:48 -0500

    se renombro por problemas de windows

--- middleware/aux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm asd.txt
-watch -n 0.5 "(date +TIME:%H:%M:%S;echo Servidor1;curl --connect-timeout 5 192.168.0.9:3000; echo '';echo Servidor2;curl --connect-timeout 5 192.168.0.12:3000; echo '';echo Servidor3;curl --connect-timeout 5 192.168.0.16:3000; echo '';) >> asd.txt"
